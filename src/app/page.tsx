f796e92079fb692f57d5b801222c1e25ec758bff
s-uxun <serenasol@ewha.ac.kr>
2025-01-07 20:30:19 +0900

    Feat: Topbar에 라우터 추가 및 포인터 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import TopBar from "@common/TopBar";
 export default function Home() {
   return (
     <div>
-      <TopBar title="탑바" />
+      <TopBar title="커피딜 지도" />
     </div>
   );
 }
---
+++ src/common/HomeTap.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { Logo } from "@assets/icons";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const HomeTap = () => {
+  const pathname = usePathname();
+
+  const tapClass = (isActive: boolean) =>
+    `w-1/2 flex justify-center items-center pb-3 border-b-[0.125rem] ${
+      isActive ? "border-black" : "border-Grey-300 text-Grey-300"
+    }`;
+
+  return (
+    <>
+      <div className="flex justify-between items-center py-3 px-5">
+        <Logo className="w-11" />
+        <Link
+          className="Caption_bold py-[0.31rem] px-[0.88rem] border border-solid border-black rounded-3xl"
+          href={"/mypage"}
+        >
+          마이페이지
+        </Link>
+      </div>
+      <div className="Body_1_bold flex mt-2">
+        <Link href={"/home"} className={tapClass(pathname === "/home")}>
+          HOME
+        </Link>
+        <Link href={"/card"} className={tapClass(pathname === "/card")}>
+          구독권
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default HomeTap;
