c2e4c9556d0d6051438dc050232be884ee82dbfb
JiwonChoi0805 <won0805@ewhain.net>
2025-01-14 20:58:21 +0900

    feat: 머지 충돌 해결

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,5 +1,4 @@
 export { default as Back } from "./Back";
-<<<<<<< HEAD
 export { default as CoffeeBeanMarker } from "./CoffeeBeanMarker";
 export { default as Crosshair } from "./Crosshair";
 export { default as EmptyHeart } from "./EmptyHeart";
@@ -8,12 +7,9 @@ export { default as HeartMarker } from "./HeartMarker";
 export { default as Logo } from "./Logo";
 export { default as PinMarker } from "./PinMarker";
 export { default as Search } from "./Search";
-=======
 export { default as BannerCat } from "./BannerCat";
 export { default as BannerCup } from "./BannerCup";
 export { default as BannerInit } from "./BannerInit";
 export { default as Front } from "./Front";
-export { default as Logo } from "./Logo";
 export { default as LogoText } from "./LogoText";
 export { default as Map } from "./Map";
->>>>>>> 8c172c8e78c2849fbed613d2089467df85bede17
