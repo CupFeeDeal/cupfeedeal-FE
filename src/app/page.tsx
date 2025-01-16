ff52c74ef52972707a909e71b7fe11584e590751
s-uxun <serenasol@ewha.ac.kr>
2025-01-17 01:13:32 +0900

    Feat: 로그인 페이지 퍼블리싱

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
+import { Logo } from "@assets/icons";
+import { LogoText } from "@assets/icons";
+import { SpeachBubble } from "@assets/icons";
+import { Kakao } from "@assets/icons";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <>
-      <div>HOME</div>
-    </>
+    <div className="h-full flex flex-col justify-center">
+      {/* 로고 */}
+      <div className="flex flex-col items-center gap-2 my-40">
+        <Logo className="w-24 " />
+        <LogoText className="w-32 " />
+      </div>
+
+      {/* 로그인 버튼 */}
+      <div className="flex flex-col items-center px-5 gap-4">
+        <SpeachBubble className="relative drop-shadow-basic" />
+        <div className="flex justify-center items-center gap-2 w-full h-12 bg-[#F5E14B] rounded-xl">
+          <Kakao />
+          <p className="Body_1_med">카카오톡으로 계속하기</p>
+        </div>
+        <Link
+          href={"/onboarding"}
+          className="Body_2_reg text-Grey-500 underline w-fit "
+        >
+          먼저 둘러보고 싶어요
+        </Link>
+      </div>
+    </div>
   );
 }
---
+++ src/assets/icons/SpeachBubble.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgSpeachBubble = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 200 77"
+    width="12.5rem"
+    {...props}
+  >
+    <path
+      fill="#fff"
+      fillRule="evenodd"
+      d="M1.09 5.46C0 7.6 0 10.4 0 16v31c0 5.6 0 8.4 1.09 10.54a10 10 0 0 0 4.37 4.37C7.6 63 10.4 63 16 63h75.262l7.506 13c.77 1.333 2.694 1.333 3.464 0l7.506-13H184c5.601 0 8.401 0 10.54-1.09a10 10 0 0 0 4.37-4.37C200 55.4 200 52.6 200 47V16c0-5.6 0-8.4-1.09-10.54a10 10 0 0 0-4.37-4.37C192.401 0 189.601 0 184 0H16C10.4 0 7.6 0 5.46 1.09a10 10 0 0 0-4.37 4.37"
+      clipRule="evenodd"
+    />
+    <foreignObject x="0" y="0" width="100%" height="100%">
+      <div className="self-center mt-4">
+        <p className="Caption_med text-center">
+          간편하게 로그인하고
+          <br />
+          오늘부터 커피값을 아껴봐요
+        </p>
+      </div>
+    </foreignObject>
+  </svg>
+);
+export default SvgSpeachBubble;
