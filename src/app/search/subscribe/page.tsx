8a1a438d5f6045109ed80e79bf19423fc6c6632f
s-uxun <serenasol@ewha.ac.kr>
2025-01-26 00:31:45 +0900

    fix: 바텀시트 디자인 수정

--- src/app/search/subscribe/page.tsx
+++
@@ -1,9 +0,0 @@
-import TopBar from "@common/TopBar";
-
-export default function Subscribe() {
-  return (
-    <div className="flex w-full flex-col">
-      <TopBar title="구독권 구매" />
-    </div>
-  );
-}
