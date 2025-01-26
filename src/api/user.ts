f6c3475e17eadd0495df36778a0d6ad37f276b02
JiwonChoi0805 <won0805@ewhain.net>
2025-01-26 14:30:41 +0900

    feat: search, user api Promise 추가

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,21 +1,15 @@
-import { Cupcat } from "src/types/mypage";
 import { privateApi } from "./client";
+import { MyInfo, Subscription, CupcatList } from "src/types/mypage";
 
 export const userApi = {
   // 마이페이지 메인 정보
-  getMyInfo: async () => {
-    const response = await privateApi.get<{
-      username: string;
-      user_level: number;
-      cupcatImgUrl: string;
-      cafe_name: string;
-      birth_date: string;
-    }>(`/api/v1/users`);
+  getMyInfo: async (): Promise<MyInfo> => {
+    const response = await privateApi.get<MyInfo>(`/api/v1/users`);
     return response.result;
   },
 
   // 마이페이지 닉네임 수정
-  patchNickname: async (username: string) => {
+  patchNickname: async (username: string): Promise<{ username: string }> => {
     const response = await privateApi.patch<{
       username: string;
     }>(`/api/v1/users`, { username: username });
@@ -23,28 +17,16 @@ export const userApi = {
   },
 
   // 마이페이지 구독 내역
-  getSubsList: async () => {
-    const response = await privateApi.get<
-      {
-        user_subscription_id: number;
-        cafe_name: string;
-        menu: string;
-        cafe_subscription_name: string;
-        period: number;
-        price: number;
-        start: string;
-        end: string;
-        status: string;
-      }[]
-    >(`/api/v1/userSubscription/manage`);
+  getSubsList: async (): Promise<Subscription[]> => {
+    const response = await privateApi.get<Subscription[]>(
+      `/api/v1/userSubscription/manage`
+    );
     return response.result;
   },
 
   // 마이페이지 지나간 컵캣
-  getCupcats: async () => {
-    const response = await privateApi.get<{ level: number; cupcats: Cupcat[] }>(
-      `/api/v1/users/cupcats`
-    );
+  getCupcats: async (): Promise<CupcatList> => {
+    const response = await privateApi.get<CupcatList>(`/api/v1/users/cupcats`);
     return response.result;
   },
 };
--- src/types/mypage.ts
+++ src/types/mypage.ts
@@ -1,3 +1,23 @@
+export interface MyInfo {
+  username: string;
+  user_level: number;
+  cupcatImgUrl: string;
+  cafe_name: string;
+  birth_date: string;
+}
+
+export interface Subscription {
+  user_subscription_id: number;
+  cafe_name: string;
+  menu: string;
+  cafe_subscription_name: string;
+  period: number;
+  price: number;
+  start: string;
+  end: string;
+  status: string;
+}
+
 export interface HistoryItem {
   id: number;
   name: string;
@@ -13,4 +33,9 @@ export interface HistoryItem {
 export interface Cupcat {
   cafe_name: string;
   cupcat_img_url: string;
+}
+
+export interface CupcatList {
+  level: number;
+  cupcats: Cupcat[];
 }
